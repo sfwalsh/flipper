9d7e7c688ab6fd0ff3be07533e48476bbfbb22db
Stephen Walsh <sfwalshappdev@gmail.com>
2020-09-03 17:35:37 +0100

    init

---
+++ Flipper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flipper
+//
+//  Created by Stephen Walsh on 03/09/2020.
+//  Copyright © 2020 Stephen Walsh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
