c65e408f4d228e8d4424428d838deb192396df58
Stephen Walsh <sfwalshappdev@gmail.com>
2020-09-03 17:43:40 +0100

    change to uikit base

---
+++ Flipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flipper
+//
+//  Created by Stephen Walsh on 03/09/2020.
+//  Copyright © 2020 Stephen Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
