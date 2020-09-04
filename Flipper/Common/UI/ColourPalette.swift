dd271200c36bcfcffc4d443057b8b593e84b991e
Stephen Walsh <sfwalshappdev@gmail.com>
2020-09-04 13:46:31 +0100

    added colour palette and font machine

---
+++ Flipper/Common/UI/ColourPalette.swift
@@ -0,0 +1,19 @@
+//
+//  ColourPalette.swift
+//  Flipper
+//
+//  Created by Stephen Walsh on 04/09/2020.
+//  Copyright © 2020 Stephen Walsh. All rights reserved.
+//
+
+import UIKit
+
+struct ColourPalette {
+    static let gregariousGreen = UIColor(red: 83/255, green: 217/255, blue: 113/255, alpha: 1.0)
+    static let lovelyLilac = UIColor(red: 250/255, green: 250/255, blue: 255/255, alpha: 1.0)
+    
+    struct Text {
+        static let primaryBlack = UIColor(red: 40/255, green: 40/255, blue: 40/255, alpha: 1.0)
+        static let secondaryBlack = UIColor(red: 40/255, green: 40/255, blue: 40/255, alpha: 0.6)
+    }
+}
